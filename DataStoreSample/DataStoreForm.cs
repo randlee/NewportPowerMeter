d0392d890343f9fc532149bc88a0ad30e0bf697b
randlee <github@randlee.com>
2016-08-31 16:08:46 -0700

    clean-up

--- DataStoreSample/DataStoreForm.cs
+++ DataStoreSample/DataStoreForm.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 using Newport.Usb;
 
